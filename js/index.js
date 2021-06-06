6227f750fa28136effefc1d7b1cee2bf641d5d2b
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-06-06 10:50:45 -0700

    made changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let bearer_token =
-  'BQAxYZJZFsqA0Vb5QO149yzoKPBJD-qWqr0m1-3bSsWuXg3j-N4-ihjuBWiEH34vFHIjpHbbyuLg_kka9iUZys9GxK61mlHuUiyYwBs_B_-xeU3Ay6mtR5R2S-MWAKrRV7X9tJaiHUxUI-tni_DanFavZtJtqtuWRa_uj5bcxINymZZe5zoGKFVxFtbfPuAU19bGthWImMqdSr1gyPwDIFo08yEPYC48lKVOxkepZm-3hh2n00HlL8X-ROXJhkQnX4Vtkz-obPSuU7NFsQqbKjzleg';
+  'BQD8etJWvvJBPUggeI4uJEFu92zN0SVDZFZAn1VilPd9p5qNzEdjqikKMdlD33fmelpyzRQm3HEfKvBUEQcDYjG0dx3UokUO-JoBkTxfWNpqZ83hk4tbx5veWEir8Pr-7-oTFJ8kWT8qbVyiBV_Us3yAfmtNNp9v2r-WTYxnbFvlaZopkj059ivtNmGKPnjQsAfB21CM_EHguv2G0uzOtV24em3JvpIvcPyTvs8KzDPuGMywmrckQlFS2MNtqUIdDTF1pLi_bIa2n2ww8r1LJqwmOQ';
 let url = 'https://api.spotify.com/v1/shows';
 let bearer = 'Bearer ' + bearer_token;
 
@@ -55,7 +55,7 @@ function fetch_latest() {
     .then((data) => {
       data.shows.forEach((show) => {
         let show_html = `
-            <div class='show' onClick='location.href = "${show.external_urls.spotify}"'>
+            <div class='show' onClick='location.href = "${show.id}"'>
                 <img src='${show.images[1].url}' />
                 <div>
                     <h4>${show.name}</h4>
@@ -72,4 +72,19 @@ function fetch_latest() {
 function fetch_all() {
   fetch_featured();
   fetch_latest();
+}
+
+function get_show(id) {
+  fetch(url + '/' + id + '?market=US', {
+    method: 'GET',
+    headers: {
+      Authorization: bearer,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .then(console.log);
 }
